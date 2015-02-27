664f57ad368f91b39a25f323bb24a88e7bf741d1
Ionică Bizău <bizauionica@yahoo.com>
2015-02-27 13:11:28 +0200

    Added module files

---
+++ test/index.js
@@ -0,0 +1,41 @@
+// Dependencies
+var IsThere = require("../lib");
+
+// Paths to test
+var paths = [
+    // exist
+    "dir"
+  , "dir/another"
+  , "dir/another/file"
+  , "dir/emptydir"
+  , "dir/file"
+  , "file"
+  , "file.ext"
+    // don't exist
+  , "foo"
+  , "foo/bar"
+  , "foo.bar"
+  , "foo/bar.foo"
+].map(function (c) {
+    return __dirname + "/contents/" + c;
+});
+
+// Sync
+console.log("> Testing sync method.");
+paths.forEach(function (c) {
+    console.log("> %s %s", c, IsThere.sync(c) ? "exists" : "doesn't exist");
+});
+
+
+console.log("> Testing async method.");
+function doSeq(i) {
+    i = i || 0;
+    var cPath = paths[i];
+    if (!cPath) { return; }
+    IsThere(cPath, function (exists) {
+        console.log("> %s %s", cPath, exists ? "exists" : "doesn't exist");
+        doSeq(i + 1);
+    });
+}
+
+doSeq();
